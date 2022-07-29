d172838cb857c93bfdac4c96a259de39d3cecaa1
Saytzeff-D <davidatanda2020@gmail.com>
2022-07-29 17:24:22 +0100

    now getting all purchases

--- controllers/purchase.controller.js
+++ controllers/purchase.controller.js
@@ -2,14 +2,24 @@ const PurchaseModel = require("../models/purchases.model")
 
 const addPurchases = (req, res)=>{
     let purchase = req.body
+    purchase.created  = new Date().toISOString()
     let form = new PurchaseModel(purchase)
     form.save((err)=>{
         if(err){
-            throw err
+            res.json({err})
         }else{
             res.json({message: 'Purchase Added Successfully'})
         }
     })
 }
+const getPurchases = (req, res)=>{
+    PurchaseModel.find((err, purchases)=>{
+        if (!err) {
+            res.json({purchases})
+        } else {
+            res.status(300).json({message: 'Server Error'})
+        }
+    })
+}
 
-module.exports = { addPurchases }
+module.exports = { addPurchases, getPurchases }
